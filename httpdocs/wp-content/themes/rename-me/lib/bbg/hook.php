e1b54bc4388a8f434d0b22069b15b85010e2c36d
Josh Stoik <josh@blobfolio.com>
2017-11-17 09:02:31 -0800

    remove page-specific code (plugin does that) move js to footer fix dependency order w/ core and libs

--- httpdocs/wp-content/themes/rename-me/lib/bbg/hook.php
+++ httpdocs/wp-content/themes/rename-me/lib/bbg/hook.php
@@ -45,35 +45,12 @@ class hook extends \bbg\wp\common\base\hook {
 
 		// Core CSS.
 		wp_register_style(
-			'bbg-core',
+			'bbg-core-css',
 			"{$css_url}core.css",
 			array(),
 			BBG_THEME_ASSET_VERSION
 		);
-		wp_enqueue_style('bbg-core');
-
-		// Some styles are page-specific. They'll be enqueued
-		// automatically so long as {type}-{slug}.css exists in the
-		// single/ folder.
-		global $template;
-		$file_slug = basename($template);
-		$file_slug = preg_replace('/\.php$/i', '', $file_slug);
-		$file_slug .= '.css';
-
-		if (file_exists($css_path . $file_slug)) {
-
-			$clean_slug = md5($file_slug);
-
-			wp_register_style(
-				$clean_slug,
-				$css_url . $file_slug,
-				array(),
-				BBG_THEME_ASSET_VERSION
-			);
-
-			wp_enqueue_style($clean_slug);
-
-		} // endif file exists
+		wp_enqueue_style('bbg-core-css');
 	}
 
 	/**
@@ -87,45 +64,23 @@ class hook extends \bbg\wp\common\base\hook {
 		$js_url = BBG_THEME_URL . 'dist/js/';
 		$js_path = BBG_THEME_PATH . 'dist/js/';
 
-		// Core JS.
-		wp_register_script(
-			'bbg-core',
-			"{$js_url}core.min.js",
-			array(),
-			BBG_THEME_ASSET_VERSION
-		);
-		wp_enqueue_script('bbg-core');
-
 		// Libs JS.
 		wp_register_script(
-			'bbg-libs',
+			'bbg-libs-js',
 			"{$js_url}libs.min.js",
 			array(),
-			BBG_THEME_ASSET_VERSION
+			BBG_THEME_ASSET_VERSION,
+			true
 		);
-		wp_enqueue_script('bbg-core');
 
-		// Some styles are page-specific. They'll be enqueued
-		// automatically so long as {type}-{slug}.css exists in the
-		// single/ folder.
-		global $template;
-		$file_slug = basename($template);
-		$file_slug = preg_replace('/\.php$/i', '', $file_slug);
-		$file_slug .= '.min.js';
-
-		if (file_exists($js_path . $file_slug)) {
-
-			$clean_slug = md5($file_slug);
-
-			wp_register_script(
-				$clean_slug,
-				$js_url . $file_slug,
-				array(),
-				BBG_THEME_ASSET_VERSION
-			);
-
-			wp_enqueue_script($clean_slug);
-
-		} // endif file exists
+		// Core JS.
+		wp_register_script(
+			'bbg-core-js',
+			"{$js_url}core.min.js",
+			array('bbg-libs-js'),
+			BBG_THEME_ASSET_VERSION,
+			true
+		);
+		wp_enqueue_script('bbg-core-js');
 	}
 }
